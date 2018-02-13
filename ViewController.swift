822d0b53ae3ca6181a6fa20ef44b9157fd4a4b1b
aakashtha49 <Newarpunk@Akashs-MacBook-Pro.local>
2018-02-13 09:18:49 +0545

    Files for Dicee-Game

---
+++ ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Newarpunk on 2/2/18.
+//  Copyright © 2018 Akash Sth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        //print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+        
+    }
+    
+}
+
